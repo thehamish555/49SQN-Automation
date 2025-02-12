02830893872f4093984cd111bca84547e6cee88f
Hamish Lester <hamishlester555@gmail.com>
2025-02-13 02:51:37 +1300

    allow deleting/changing emails

--- frontend/website/pages/accounts/manage_users.py
+++ frontend/website/pages/accounts/manage_users.py
@@ -14,7 +14,7 @@ edited_data = st.data_editor(df,
                              num_rows='dynamic')
 
 data = edited_data.to_dict()
-paired_data = [{"email": data["email"][i], "role": data["role"][i]} for i in range(len(data["email"]))]
+paired_data = [{'email': data['email'][key], 'role': data['role'][key]} for key in data['email'].keys()]
 if paired_data != st.session_state.users.data:
     try:
         execute_query(
@@ -23,8 +23,20 @@ if paired_data != st.session_state.users.data:
             ),
             ttl=0,
         )
+        existing_emails = {row['email'] for row in st.session_state.users.data}
+        updated_emails = {row['email'] for row in paired_data}
+        emails_to_delete = existing_emails - updated_emails
+        for email in emails_to_delete:
+            execute_query(
+                st.session_state.conn.table('users')
+                .delete()
+                .eq('email', email),
+                ttl=0,
+            )
+
         st.session_state.conn = None
         st.session_state.pop('users')
         st.session_state.pop('user')
-    except:
+        st.rerun()
+    except Exception as e:
         st.error('Must provide an email address')
