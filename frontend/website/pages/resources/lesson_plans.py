1084b484d24e9b250a1873b1d5f045a47d67f308
Hamish Lester <hamishlester555@gmail.com>
2025-02-13 16:49:01 +1300

    lesson plans now on the cloud

--- frontend/website/pages/resources/lesson_plans.py
+++ frontend/website/pages/resources/lesson_plans.py
@@ -2,29 +2,35 @@ import streamlit as st
 import streamlit_pdf_viewer as pdf_viewer
 import pymupdf
 import io
-import os
+import requests
 
 cols = st.columns([1, 3, 2], border=True)
-if st.session_state.is_local:
-    path = 'resources/lesson_plans'
-else:
-    path = './frontend/website/resources/lesson_plans'
 
-files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-files.sort(key=lambda x: (not x.endswith('Template.pdf'), x))
+
+if 'files' not in st.session_state:
+    st.session_state.files = st.session_state.conn.create_signed_urls('lesson_plans', [file['name'] for file in st.session_state.conn.list_objects('lesson_plans')], expires_in=3600)
 if 'file' not in st.session_state:
     st.session_state['file'] = None
+if 'file_name' not in st.session_state:
+    st.session_state['file_name'] = None
 if 'last_file' not in st.session_state:
     st.session_state['last_file'] = None
 if 'pdf' not in st.session_state:
     st.session_state['pdf'] = None
 
+st.session_state.files.sort(key=lambda x: (not x['path'].endswith('Template.pdf'), x['path']))
 icons = {'Default': ':material/docs:', 'Template': ':material/docs:', 'Bush Craft': ':material/forest:',}
 
 @st.cache_data
-def load_file(file):
-    with open(f'{path}/{file}', 'rb') as pdf_file:
-        return pdf_file.read()
+def get_data(file):
+    try:
+        response = requests.get(file['signedURL'])
+        if response.status_code == 200:
+            return io.BytesIO(response.content).getvalue()
+    except TypeError:
+        response = requests.get(file)
+        if response.status_code == 200:
+            return io.BytesIO(response.content).getvalue()
 
 
 def set_large_pdf(annotation):
@@ -32,7 +38,8 @@ def set_large_pdf(annotation):
         st.session_state['last_file'] = annotation['file']
     else :
         st.session_state['last_file'] = None
-    st.session_state['file'] = f'{path}/{annotation['file']}'
+    st.session_state['file'] = get_data(annotation['file'])
+    st.session_state['file_name'] = annotation['name']
     st.rerun()
 
 
@@ -42,7 +49,7 @@ def unload_file():
 
 
 try:
-    @st.dialog(st.session_state['file'].split('/')[-1].removesuffix('.pdf'), width="large")
+    @st.dialog(st.session_state['file_name'].removesuffix('.pdf'), width="large")
     def view_large_pdf():
         pdf_viewer.pdf_viewer(st.session_state['file'], width=1000, render_text=True)
 except AttributeError:
@@ -52,21 +59,22 @@ except AttributeError:
 with cols[0]:
     st.markdown('#### View Lesson Plans')
     search = st.text_input('Search', key='search', placeholder='Search for Lesson Plans...')
-    files = [f for f in files if search.lower() in f.lower().removesuffix('.pdf')]
+    files = [f for f in st.session_state.files if search.lower() in f['path'].lower().removesuffix('.pdf')]
     st.caption(f'*Found {len(files)} files*')
     for file in files:
         try:
-            icon = icons[file.split(' - ')[1].removesuffix('.pdf')]
+            icon = icons[file['path'].split(' - ')[1].removesuffix('.pdf')]
         except (KeyError, IndexError):
             icon = icons['Default']
-        with st.expander(file.removesuffix('.pdf'), icon=icon):
-            pdf_viewer.pdf_viewer(f'{path}/{file}', width=350, pages_to_render=[1], on_annotation_click=set_large_pdf, annotation_outline_size=0, annotations=[{'page': 1, 'x': 0, 'y': 0, 'width': 600, 'height': 845, 'file': file, 'last_file': st.session_state['last_file']}])
-            st.download_button('Download as PDF', data=load_file(file), file_name=file, mime='application/octet-stream', icon=':material/download:')
+        with st.expander(file['path'].removesuffix('.pdf'), icon=icon):
+            file_data = get_data(file)
+            pdf_viewer.pdf_viewer(file_data, width=350, pages_to_render=[1], on_annotation_click=set_large_pdf, annotation_outline_size=0, annotations=[{'page': 1, 'x': 0, 'y': 0, 'width': 600, 'height': 845, 'file': file['signedURL'], 'name': file['path'], 'last_file': st.session_state['last_file']}])
+            st.download_button('Download as PDF', data=file_data, file_name=file['path'], mime='application/octet-stream', icon=':material/download:')
 
 with cols[1]:
     st.markdown('#### Auto fill Lesson Plan')
     with st.form(key='create_lesson', border=False, enter_to_submit=False):
-        selected_file = st.selectbox('Select a Lesson Plan', [f.removesuffix('.pdf') for f in files if not f.endswith('Template.pdf')])
+        selected_file = st.selectbox('Select a Lesson Plan', [f['path'].removesuffix('.pdf') for f in st.session_state.files if not f['path'].endswith('Template.pdf')])
         date = st.date_input('Date', format="DD/MM/YYYY", value='today', min_value='today')
         instructor = st.text_input('Instructor', placeholder='Enter the Instructors Name (Rank + Last Name or Full Name)...', value=st.experimental_user.name, max_chars=50)
 
@@ -75,8 +83,7 @@ with cols[1]:
                 '[DATE]': date.strftime('%d/%m/%Y'),
                 '[INSTRUCTOR]': instructor
             }
-
-            st.session_state['pdf'] = pymupdf.open(f'{path}/{selected_file}.pdf')
+            st.session_state['pdf'] = pymupdf.open(stream=get_data(st.session_state.files[[f['path'] for f in st.session_state.files].index(selected_file + '.pdf')]))
             for page in st.session_state['pdf']:
                 for key, value in replacements.items():
                     text_instances = page.search_for(key)
