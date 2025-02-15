89e77d2fd5a7fea9aa7634ee64f65e33ac99f34b
Hamish Lester <hamishlester555@gmail.com>
2025-02-16 03:39:56 +1300

    Add help messages to certain widgets

--- frontend/website/sub_pages/resources/lesson_plans.py
+++ frontend/website/sub_pages/resources/lesson_plans.py
@@ -54,10 +54,10 @@ def confirmation(file):
     st.write('This action cannot be undone.')
     cols = st.columns(2)
     with cols[0]:
-        if st.button('Cancel', use_container_width=True):
+        if st.button('Cancel', use_container_width=True, help='Cancel the deletion'):
             st.rerun()
     with cols[1]:
-        if st.button('**:red[Delete]**', use_container_width=True):
+        if st.button('**:red[Delete]**', use_container_width=True, help='Delete the lesson plan'):
             st.session_state.conn.remove('lesson_plans', [f'{file}.pdf'])
             st.session_state.pop('files')
             st.session_state.conn = None
@@ -66,9 +66,9 @@ def confirmation(file):
 
 
 with cols[0]:
-    st.markdown('#### Lesson Plan Viewer')
-    st.toggle('Display as links', key='display_as_links', value=False)
-    search = st.text_input('Search', key='search', placeholder='Search for Lesson Plans...')
+    st.write('#### Lesson Plan Viewer')
+    st.toggle('Display as links', key='display_as_links', value=False, help='Toggle between displaying lesson plans as links or buttons')
+    search = st.text_input('Search', key='search', placeholder='Search for Lesson Plans...', help='Search for a specific lesson plan')
     files = [f for f in st.session_state.files if search.lower() in f['path'].lower().removesuffix('.pdf')]
     st.caption(f'*Showing {len(files[:10])}/{len(st.session_state.files)} files*')
     for file in files[:10]:
@@ -79,18 +79,18 @@ with cols[0]:
         if st.session_state.display_as_links:
             st.write(f'{icon} [{file['path'].removesuffix('.pdf')}]({urllib.parse.quote(file['signedURL'], safe=":/?=&")})')
         else:
-            if st.button(file['path'].removesuffix('.pdf'), icon=icon, type='tertiary'):
+            if st.button(file['path'].removesuffix('.pdf'), icon=icon, type='tertiary', help='View this lesson plan'):
                 view_large_pdf(get_data(file), file['path'])
 
 with cols[1]:
-    tabs = st.tabs(['Auto Fill Lesson Plans', 'Lesson Plan Manager'])
+    tabs = st.tabs(['Autofill Lesson Plans', 'Lesson Plan Manager'])
     with tabs[0]:
         with st.form(key='create_lesson', border=False, enter_to_submit=False):
-            selected_file = st.selectbox('Select a Lesson Plan', [f['path'].removesuffix('.pdf') for f in st.session_state.files if not f['path'].endswith('Template.pdf')])
-            date = st.date_input('Date', format="DD/MM/YYYY", value='today', min_value='today')
-            instructor = st.text_input('Instructor', placeholder='Enter the Instructors Name (Rank + Last Name or Full Name)...', value=st.experimental_user.name, max_chars=50)
+            selected_file = st.selectbox('Select a Lesson Plan', [f['path'].removesuffix('.pdf') for f in st.session_state.files if not f['path'].endswith('Template.pdf')], help='Select a lesson plan to autofill')
+            date = st.date_input('Date', format="DD/MM/YYYY", value='today', min_value='today', help='Select the date for the lesson plan')
+            instructor = st.text_input('Instructor', placeholder='Enter the Instructors Name (Rank + Last Name or Full Name)...', value=st.experimental_user.name, max_chars=50, help='Enter the name of the instructor for the lesson plan')
 
-            if st.form_submit_button('Create Lesson Plan', use_container_width=True):
+            if st.form_submit_button('Create Lesson Plan', use_container_width=True, help='Create a lesson plan with the selected template'):
                 replacements = {
                     '[DATE]': date.strftime('%d/%m/%Y'),
                     '[INSTRUCTOR]': instructor
@@ -110,12 +110,12 @@ with cols[1]:
         try:
             pdf_bytes = io.BytesIO()
             st.session_state['pdf'].save(pdf_bytes)
-            st.success(f'Lesson Plan *"{selected_file}"* Created!')
+            st.success(f'Lesson Plan *"{selected_file}"* Created!', icon=':material/check:')
             sub_cols = st.columns(2)
             with sub_cols[0]:
-                st.download_button('Download as PDF', data=pdf_bytes.getvalue(), file_name=f'{selected_file} - {date.strftime("%d-%m-%Y")}.pdf', mime='application/octet-stream', icon=':material/download:', use_container_width=True)
+                st.download_button('Download as PDF', data=pdf_bytes.getvalue(), file_name=f'{selected_file} - {date.strftime("%d-%m-%Y")}.pdf', mime='application/octet-stream', icon=':material/download:', use_container_width=True, help='Download')
             with sub_cols[1]:
-                if st.button('Preview', icon=':material/visibility:', use_container_width=True):
+                if st.button('Preview', icon=':material/visibility:', use_container_width=True, help='Preview the lesson plan'):
                     view_large_pdf(pdf_bytes.getvalue(), selected_file)
             pdf_bytes.seek(0)
         except AttributeError:
@@ -124,14 +124,14 @@ with cols[1]:
     with tabs[1]:
         if st.session_state.user['role'] == 'admin':
             with st.form(key='submit_lesson_plan', enter_to_submit=False):
-                uploaded_pdf = st.file_uploader('Upload a Lesson Plan', type=['pdf'])
-                pdf_name = st.text_input('Lesson Plan Name', placeholder='Enter the Lesson Plan Name...', max_chars=50)
-                lesson_type = st.selectbox('Lesson Plan Type', [key for key in icons.keys() if key != 'Default'])
-                if st.form_submit_button('Upload Lesson Plan'):
+                uploaded_pdf = st.file_uploader('Upload a Lesson Plan', type=['pdf'], help='Select a lesson plan to upload')
+                pdf_name = st.text_input('Lesson Plan Name', placeholder='Enter the Lesson Plan Name...', max_chars=50, help='Enter a name for the lesson plan')
+                lesson_type = st.selectbox('Lesson Plan Type', [key for key in icons.keys() if key != 'Default'], help='Select the type of lesson plan')
+                if st.form_submit_button('Upload Lesson Plan', help='Upload the lesson plan to the database'):
                     if not uploaded_pdf:
-                        st.error('Please upload a PDF file')
+                        st.error('Please upload a PDF file', icon=':material/error:')
                     elif pdf_name.strip() == '':
-                        st.error('Please enter a name for the Lesson Plan')
+                        st.error('Please enter a name for the Lesson Plan', icon=':material/error:')
                     else:
                         try:
                             st.session_state.conn.upload('lesson_plans', source='local', file=uploaded_pdf, destination_path=f'/{pdf_name} - {lesson_type}.pdf')
@@ -139,15 +139,16 @@ with cols[1]:
                             st.session_state.conn = None
                             st.rerun()
                         except Exception as e:
-                            st.error('File already exists, try another name')
+                            st.error('File already exists, try another name', icon=':material/error:')
             with st.form(key='remove_lesson_plan'):
-                selected_file = st.selectbox('Select a Lesson Plan to Remove', [f['path'].removesuffix('.pdf') for f in st.session_state.files])
-                if st.form_submit_button('**:red[Remove Lesson Plan]**'):
+                selected_file = st.selectbox('Select a Lesson Plan to Remove', [f['path'].removesuffix('.pdf') for f in st.session_state.files], help='Select a lesson plan to remove')
+                if st.form_submit_button('**:red[Remove Lesson Plan]**', help='Remove the selected lesson plan'):
                     confirmation(selected_file)
         else:
             st.warning('You do not have permission to access this tab')
 '---'
-st.markdown('#### Quick Links')
+
+st.write('#### Quick Links')
 st.write('- [Cadet Net Lesson Plans](https://www.cadetnet.org.nz/7-training/lesson-plans/)')
 st.write('- [Cadet Net Training Manuals](https://www.cadetnet.org.nz/7-training/training-manuals/)')
 st.write('- [Cadet Net Training Resources](https://www.cadetnet.org.nz/7-training/training-resources/)')
