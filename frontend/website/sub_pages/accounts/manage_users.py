2731d0777caf3638be898a105da30d1771d4bdff
Hamish Lester <hamishlester555@gmail.com>
2025-02-20 02:50:34 +1300

    Add names to user DB

--- frontend/website/sub_pages/accounts/manage_users.py
+++ frontend/website/sub_pages/accounts/manage_users.py
@@ -28,29 +28,33 @@ def confirmation(email):
 
 with cols[0]:
     st.write('### View Users')
+    st.write('')
     edited_data = st.dataframe(df,
                             column_config={
+                                'name': st.column_config.TextColumn('Name', help='The name of the user'),
                                 'email': st.column_config.TextColumn('Email', help='The email address of the user'),
                                 'permissions': st.column_config.ListColumn('Permissions', help='The permissions of the user')
                                 },
+                            column_order=['name', 'email', 'permissions'],
                             hide_index=True,
                             use_container_width=True,
-                            height=275)
+                            height=325)
 
 with cols[1]:
     st.write('### Manage Users')
     tabs = st.tabs(['Add Users', 'Delete Users', 'Edit Users'])
     with tabs[0]:
         with st.form(key='create_user', border=False, enter_to_submit=False):
+            name = st.text_input('Name', placeholder='Name', help='Enter the name of the new user')
             email = st.text_input('Email', placeholder='Email', help='Enter the email address of the new user')
             permissions = st.multiselect('Permissions', options=['manage_lesson_plans', 'manage_users', 'admin'], placeholder='Select Permissions', help='Select the permissions for the new user')
-            if st.form_submit_button('Create User'):
+            if st.form_submit_button('Create User', help='Create a new user with the provided details'):
                 email = email.lower()
                 if email:
                     try:
                         execute_query(
                             st.session_state.conn.table('users').insert(
-                                [{'email': email, 'permissions': permissions}],
+                                [{'name': name, 'email': email, 'permissions': permissions}],
                                 count='None'
                             ),
                             ttl=0
@@ -69,11 +73,12 @@ with cols[1]:
             confirmation(selected_email)
     with tabs[2]:
         selected_email = st.selectbox('Select a User to Edit', [row['email'] for row in st.session_state.users.data])
+        name = st.text_input('Name', value=[row['name'] for row in st.session_state.users.data if row['email'] == selected_email][0], help='Enter the name of the selected user')
         selected_permissions = st.multiselect('Permissions', options=['manage_lesson_plans', 'manage_users', 'admin'], default=[row['permissions'] for row in st.session_state.users.data if row['email'] == selected_email][0], help='Select the permissions for the selected user')
-        if st.button('Edit User'):
+        if st.button('Edit User', help='Edit the selected user with the provided details'):
             execute_query(
                 st.session_state.conn.table('users')
-                .update({'permissions': selected_permissions})
+                .update({'name': name, 'permissions': selected_permissions})
                 .eq('email', selected_email),
                 ttl=0,
             )
