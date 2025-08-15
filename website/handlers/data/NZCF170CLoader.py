4c1f73f46b813d438c5373753f389878e573ec4b
Hamish Lester <hamishlester555@gmail.com>
2025-08-15 21:12:21 +1200

    weeeeeeeeeeee

--- website/handlers/data/NZCF170CLoader.py
+++ website/handlers/data/NZCF170CLoader.py
@@ -34,11 +34,7 @@ class NZCF170CLoader:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--log-level=3")
-        chrome_options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/115.0.0.0 Safari/537.36"
-        )
+        chrome_options.add_argument("--silent")
 
         # Initialize driver
         self.driver = webdriver.Chrome(
